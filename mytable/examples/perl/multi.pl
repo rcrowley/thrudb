7afe04e2a081fdeb2cc006185e13ae3c0041c020
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2007-12-29 03:17:49 +0000

    slightly improved w/timing and such. still needs work
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@75 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- mytable/examples/perl/multi.pl
+++ mytable/examples/perl/multi.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use Time::HiRes qw(time);
 
 #Thrift components
 use Thrift;
@@ -13,59 +14,66 @@ use Thrift::FramedTransport;
 
 use MyTable;
 
-#Config
 use constant MYTABLE_PORT    => 9091;
 
 use threads;
 
+my $num_threads = 15;
+my $num_calls = 100;
+
+# load up data
+eval{
+    my $socket    = new Thrift::Socket("localhost",MYTABLE_PORT());
+    my $transport = new Thrift::FramedTransport($socket);
+    my $protocol  = new Thrift::BinaryProtocol($transport);
+
+    my $mytable  = new MyTableClient($protocol);
+
+    $transport->open();
+
+    foreach (0..($num_calls - 1))
+    {
+        $mytable->put ("data", "key.$_", "value_$_");
+    }
+};
+if($@) {
+    die Dumper ($@) if UNIVERSAL::isa($@,"Thrift::TException");
+    die $@;
+}
+
 my @threads = ();
-foreach (0..10)
+foreach (0..($num_threads - 1))
 {
     push @threads, threads->new (\&make_calls);
 }
 
+my $start = time;
 foreach (@threads)
 {
     $_->join;
 }
+my $end = time;
+my $dur = $end - $start;
+printf "%f -> %f = %f\n", $start, $end, $dur;
+my $total_calls = $num_threads * $num_calls * 10;
+my $tps = $total_calls / $dur;
+printf "%d / %f = %f\n", $total_calls, $dur, $tps;
+
 
 sub make_calls
 {
-    my $mytable;
     eval{
         my $socket    = new Thrift::Socket("localhost",MYTABLE_PORT());
         my $transport = new Thrift::FramedTransport($socket);
         my $protocol  = new Thrift::BinaryProtocol($transport);
 
-        $mytable  = new MyTableClient($protocol);
+        my $mytable  = new MyTableClient($protocol);
 
         $transport->open();
-    }; if($@) {
-        die $@->{message} if UNIVERSAL::isa($@,"Thrift::TException");
-        die $@;
-    }
 
-    eval {
-
-        my $id = $mytable->put ("partitions", "key3", "val-key4");
-        my $key = "key.".rand;
-        $mytable->put ("partitions", $key, "val.$key");
-
-        print Dumper ($mytable->get ("partitions", "key3"));
-        print Dumper ($mytable->get ("partitions", $key));
-
-        if (rand (100) > 50)
+        foreach (0..(($num_calls - 1) * 10))
         {
-            $mytable->remove ("partitions", $key);
-        }
-
-        print Dumper ($mytable->get ("partitions", "key3"));
-#        print Dumper ($mytable->get ("partitions", $key));
-
-        foreach (0..100)
-        {
-            $mytable->put ("partitions", "key.$_", "value_$_");
-            $mytable->get ("partitions", "key.$_");
+            $mytable->get ("data", 'key.'.int (rand ($num_calls)));
         }
     };
     if($@) {
