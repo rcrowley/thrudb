f7cdfeefea57a2a370f898b1b4321585916e2772
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2007-12-31 21:06:32 +0000

    ignores
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@98 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- diststore/src/main.cpp
+++ diststore/src/main.cpp
@@ -1,4 +1,9 @@
 /**
+ * Copyright (c) 2007- T Jake Luciani
+ * Distributed under the New BSD Software License
+ *
+ * See accompanying file LICENSE or visit the Thrudb site at:
+ * http://thrudb.googlecode.com
  *
  **/
 
