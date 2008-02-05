ac48a82a4f2504c9099d9e416746bc7cd82b1820
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2008-02-05 17:54:40 +0000

    autoconf cleanup.  - require libmemcached >= 0.14, using pkg-config again  - added specific check for libevent, until thrift's pkg-config is fixed  - adapted fix from Stefan Görling, to warn when you don't have pkg-config  installed, only put it in thrucommong since it's a base pkg.
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@249 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- thrucommon/bootstrap.sh
+++ thrucommon/bootstrap.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+if ! pkg-config --version >/dev/null 2>&1; then
+  echo "pkg-config is required to proceed, please install and try again...exiting" >&2
+  exit 1
+fi
+
 #autoscan
 libtoolize --copy --force
 aclocal -I config
