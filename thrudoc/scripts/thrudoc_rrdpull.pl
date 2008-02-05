c0191ffa17ee77b4d3f7c7913409ea403872fa6e
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2008-02-05 09:53:10 +0000

    initial rrdtool script
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@247 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

---
+++ thrudoc/scripts/thrudoc_rrdpull.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+
+# example graph command:
+# rrdtool graph req.png --end 1202197140 --start 1202194500 --height=600    \
+#   --width=800 DEF:gets=animal.rrd:get_count:AVERAGE LINE2:gets#ff0000   \
+#   DEF:puts=animal.rrd:put_count:AVERAGE LINE2:puts#0000ff   \
+#   DEF:scan=animal.rrd:scan_count:AVERAGE LINE2:scan#00ff00  \
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Thrift;
+use Thrift::MemoryBuffer;
+use Thrift::BinaryProtocol;
+use Thrift::Socket;
+use Thrift::FramedTransport;
+use Thrudoc;
+
+my $hostname = shift @ARGV;
+my $port = 9091;
+my $interval = 60;
+
+my $thrudoc;
+eval
+{
+    my $socket    = new Thrift::Socket ($hostname, $port);
+    my $transport = new Thrift::FramedTransport ($socket);
+    my $protocol  = new Thrift::BinaryProtocol ($transport);
+
+    $thrudoc  = new ThrudocClient($protocol);
+
+    $transport->open();
+
+    create ($hostname, $interval * 2, get_stats ());
+}; 
+if($@) 
+{
+    die $@->{message} if (UNIVERSAL::isa ($@,"Thrift::TException"));
+    die $@;
+}
+
+while (1)
+{
+    eval
+    {
+        update ($hostname, get_stats ());
+    }; 
+    if($@) 
+    {
+        if (UNIVERSAL::isa($@,"Thrift::TException"))
+        {
+            warn $@->{message};
+        }
+        else
+        {
+            die $@;
+        }
+    }
+    sleep $interval;
+}
+
+sub get_stats
+{
+    my $stats = $thrudoc->admin ('stats');
+    $stats = [ split /,/, $stats ];
+    $stats = { map { split /=/ } @$stats };
+}
+
+sub create
+{
+    my ($hostname, $heartbeat, $stats) = @_;
+
+    my @dss = ();
+    foreach my $var (sort keys %$stats)
+    {
+        push @dss, sprintf ('DS:%s:COUNTER:%d:0:U', $var, $heartbeat);
+    }
+
+    my $cmd = sprintf ('rrdtool create rrdtool/%s.rrd --step 60 %s %s', $hostname,
+        join (' ', @dss), 'RRA:AVERAGE:0.5:1:60 RRA:AVERAGE:0.5:5:72 RRA:AVERAGE:0.5:60:24 RRA:AVERAGE:0.5:1440:7');
+    print $cmd."\n";
+    system ($cmd);
+}
+
+sub update
+{
+    my ($hostname, $stats) = @_;
+    my $cmd = sprintf ('rrdtool update rrdtool/%s.rrd N:%s', $hostname, 
+        join (':', map { $stats->{$_} } sort keys %$stats));
+    print $cmd."\n";
+    system ($cmd);
+}
