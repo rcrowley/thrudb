dd09cbfadafcd11c914f1796db6c183c5cd7a4f1
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2008-02-07 02:39:17 +0000

    convert to using BDB's for recording messages
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@254 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- spread_recorder/scripts/recorder.pl
+++ spread_recorder/scripts/recorder.pl
@@ -3,18 +3,36 @@
 use strict;
 use warnings;
 use Data::Dumper;
-
+use BDB;
 use Spread::Connection;
 
 my $conn = new Spread::Connection ('recorder');
 
+my $env = db_env_create;
+my $db_dir = 'recorded';
+mkdir $db_dir unless (-d $db_dir);
+
+db_env_open ($env, $db_dir, 
+    BDB::CREATE     | 
+    BDB::RECOVER    |
+    BDB::INIT_LOCK  |
+    BDB::INIT_LOG   |
+    BDB::INIT_MPOOL |
+    BDB::INIT_TXN   |
+    BDB::PRIVATE, 0600);
+
+$env->set_flags (
+    BDB::AUTO_COMMIT |
+    BDB::CREATE, 1);
+
+my %dbs;
+
 # general recorder
-my %messages;
 $conn->subscribe (undef, 'channel', 1, sub { 
         my ($conn, $sender, $group, $mess_type, $message) = @_;
-        push @{$messages{$group}}, $message;
-        printf "rec: %s - %s - %d - %s\n", $group, $sender, 
-            scalar (@{$messages{$group}}), $message;
+        db_put (db_for_group ($group), undef, message_to_key ($message),
+            $message);
+        printf "rec: %s - %s - %s\n", $group, $sender, $message;
 
         1;
     });
@@ -25,27 +43,49 @@ $conn->subscribe (undef, 'channel', 101, sub {
         printf "catch: %s - %s - %s\n", $group, $sender, $message;
 
         # find the "next" message
-        # TODO: a binary search or something like that... anything but this
-        my $group_mesg = $messages{$group};
-        my $resp = 'none';
-        if (defined $group_mesg)
+        my $cur = db_for_group ($group)->cursor (undef, BDB::READ_UNCOMMITTED);
+        my $data;
+        # find the one they passed in
+        db_c_get ($cur, message_to_key ($message), $data, BDB::SET_RANGE);
+        # TODO: error handling
+        # and move to the next
+        db_c_get ($cur, message_to_key ($message), $data, BDB::NEXT);
+        my $resp = "none";
+        if ($!)
         {
-            for (my $i = 0; $i < scalar (@$group_mesg); $i++)
-            {
-                if ($message eq $group_mesg->[$i])
-                {
-                    $resp = $group_mesg->[$i+1];
-                    # we've found the last, send back the next
-                    last;
-                }
-            }
+            warn $!."\n";
+        }
+        elsif (defined $data)
+        {
+            $resp = $data;
         }
         printf "resp: %s - %s\n", $sender, $resp;
         $conn->queue ($sender, 101, $resp);
-
-        # we didn't find their last :(
+        db_c_close ($cur);
 
         1;
     });
 
 $conn->run;
+
+sub message_to_key 
+{
+    $_[0] =~ /^(\d+\.\d+)/;
+    return $1;
+}
+
+sub db_for_group
+{
+    my ($group) = @_;
+    my $db = $dbs{$group};
+    unless ($db)
+    {
+        $db = db_create $env;
+        db_open ($db, undef, $group, undef, BDB::BTREE, 
+            BDB::AUTO_COMMIT | 
+            BDB::CREATE      |
+            BDB::READ_UNCOMMITTED, 0600);
+        $dbs{$group} = $db;
+    }
+    return $db;
+}
--- spread_recorder/scripts/sender.pl
+++ spread_recorder/scripts/sender.pl
@@ -11,7 +11,7 @@ my $conn = new Spread::Connection ('sender');
 
 while (1)
 {
-    $conn->queue ('channel', 1, time);
+    $conn->queue ('channel', 1, time . ' a message for you');
     # -1 b/c we're not listening for anything and just want to dispatch
     # any waiting messages
     $conn->run (-1);
