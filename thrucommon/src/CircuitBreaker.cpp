f3bbee01c7a814c5809eabfde127734dcafca8de
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2008-02-26 03:47:04 +0000

    added unit tests for circuit breaker. added logging and fixed found bugs
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@309 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- thrucommon/src/CircuitBreaker.cpp
+++ thrucommon/src/CircuitBreaker.cpp
@@ -14,6 +14,11 @@ LoggerPtr CircuitBreaker::logger (Logger::getLogger ("CircuitBreaker"));
 CircuitBreaker::CircuitBreaker (uint16_t threshold, 
                                 uint16_t timeout_in_seconds)
 {
+    char buf[64];
+    sprintf (buf, "CircuitBreaker: threshold=%d, timeout_in_seconds=%d",
+             threshold, timeout_in_seconds);
+    LOG4CXX_INFO (logger, buf);
+
     this->threshold = threshold;
     this->timeout_in_seconds = timeout_in_seconds;
 
@@ -22,9 +27,11 @@ CircuitBreaker::CircuitBreaker (uint16_t threshold,
 
 bool CircuitBreaker::allow ()
 {
+    LOG4CXX_DEBUG (logger, "allow: ");
     // if timeout has elapsed give half open a try
-    if (this->next_check > time (0))
+    if (this->state == OPEN && this->next_check < time (0))
     {
+        LOG4CXX_INFO (logger, "allow:    going half-open");
         this->state = HALF_OPEN;
     }
     return this->state == CLOSED || this->state == HALF_OPEN;
@@ -32,19 +39,23 @@ bool CircuitBreaker::allow ()
 
 void CircuitBreaker::success ()
 {
+    LOG4CXX_DEBUG (logger, "success: ");
     // if we're half-open and got a success close things up, note that
     // we should never be called if we're in the fully open state, that'd be a 
     // bug in the client code
     if (this->state == HALF_OPEN)
     {
+        LOG4CXX_INFO (logger, "success:    in half-open, reset");
         this->reset ();
     }
 }
 
 void CircuitBreaker::failure ()
 {
+    LOG4CXX_DEBUG (logger, "failure: ");
     if (this->state == HALF_OPEN)
     {
+        LOG4CXX_INFO (logger, "failure:    in half-open, trip");
         this->trip ();
     }
     else
@@ -53,22 +64,26 @@ void CircuitBreaker::failure ()
         // if we've breached our threshold trip
         if (failure_count > this->threshold)
         {
-            this->reset ();
+            LOG4CXX_INFO (logger, "failure:    threashold breached,  trip");
+            this->trip ();
         }
     }
 }
 
 void CircuitBreaker::reset ()
 {
+    LOG4CXX_INFO (logger, "reset:");
     this->state = CLOSED;
     this->failure_count = 0;
 }
 
 void CircuitBreaker::trip ()
 {
+    LOG4CXX_INFO (logger, "trip:");
     if (this->state != OPEN)
     {
+        LOG4CXX_INFO (logger, "trip: tripped");
         this->state = OPEN;
-        this->next_check = time (0) + timeout_in_seconds;
+        this->next_check = time (0) + (time_t)timeout_in_seconds;
     }
 }
