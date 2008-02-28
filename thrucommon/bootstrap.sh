d83c8438d7d3e4297838985a32168806663833d3
rm <rm@a07ddc84-1b2c-4fb2-82be-f96b6b69ae06>
2008-02-28 17:59:06 +0000

    include m4 files for non-common stuff so errors will come out at more approrpriate times with better messages.
    git-svn-id: http://svn.thrudb.org/thrudb/trunk@319 a07ddc84-1b2c-4fb2-82be-f96b6b69ae06

--- thrucommon/bootstrap.sh
+++ thrucommon/bootstrap.sh
@@ -1,13 +1,8 @@
 #! /bin/sh
 
-if ! pkg-config --version >/dev/null 2>&1; then
-  echo "pkg-config is required to proceed, please install and try again...exiting" >&2
-  exit 1
-fi
-
 #autoscan
 libtoolize --copy --force
-aclocal -I config
+aclocal -I config/ac-macros
 autoheader
 touch NEWS README AUTHORS ChangeLog
 automake --add-missing --copy
